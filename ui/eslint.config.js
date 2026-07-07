ligoj/plugin-scm-gitlab

feat: initial implementation

ui/eslint.config.js
@@ -1,0 +1,14 @@
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import globals from 'globals'
+
+export default [
+  { ignores: ['node_modules/**', 'dist/**', 'coverage/**', '../src/main/resources/META-INF/resources/webjars/scm-gitlab/vue/**'] },
+  js.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+  {
+    files: ['**/*.{js,mjs,cjs,vue}'],
+    languageOptions: { ecmaVersion: 'latest', sourceType: 'module', globals: { ...globals.browser, ...globals.node } },
+    rules: { 'vue/multi-word-component-names': 'off', 'vue/valid-v-slot': ['error', { allowModifiers: true }], 'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }] },
+  },
+]

ui/vite.config.js
@@ -1,0 +1,37 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+
+const HOST_SRC = resolve(__dirname, '../../../ligoj/app-ui/src/main/webapp/src')
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: { '@ligoj/host': resolve(HOST_SRC, 'host.js'), '@': HOST_SRC },
+    dedupe: ['vue', 'pinia', 'vue-router', 'vuetify'],
+  },
+  build: {
+    lib: { entry: resolve(__dirname, 'src/index.js'), formats: ['es'], fileName: () => 'index.js' },
+    outDir: resolve(__dirname, '../src/main/resources/META-INF/resources/webjars/scm-gitlab/vue'),
+    emptyOutDir: true,
+    rollupOptions: {
+      external: ['vue', 'vue-router', 'pinia', 'vuetify', '@ligoj/host'],
+      output: { assetFileNames: 'index.[ext]' },
+    },
+  },
+  server: { port: 5317, proxy: { '/rest': { target: 'http://localhost:8080', changeOrigin: true }, '/webjars': { target: 'http://localhost:8080', changeOrigin: true } } },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['src/__tests__/setup.js'],
+    exclude: ['node_modules/**', 'dist/**'],
+    css: false,
+    server: { deps: { inline: ['vuetify'] } },
+    coverage: {
+      provider: 'v8',
+      include: ['src/**/*.js'],
+      exclude: ['src/__tests__/**'],
+      thresholds: { statements: 100, branches: 100, functions: 100, lines: 100 },
+    },
+  },
+})

ligoj/plugin-registry-harbor

ui/eslint.config.js
@@ -1,0 +1,14 @@
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import globals from 'globals'
+
+export default [
+  { ignores: ['node_modules/**', 'dist/**', 'coverage/**', '../src/main/resources/META-INF/resources/webjars/registry-harbor/vue/**'] },
+  js.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+  {
+    files: ['**/*.{js,mjs,cjs,vue}'],
+    languageOptions: { ecmaVersion: 'latest', sourceType: 'module', globals: { ...globals.browser, ...globals.node } },
+    rules: { 'vue/multi-word-component-names': 'off', 'vue/valid-v-slot': ['error', { allowModifiers: true }], 'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }] },
+  },
+]

ui/vite.config.js
@@ -1,0 +1,37 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+
+const HOST_SRC = resolve(__dirname, '../../../ligoj/app-ui/src/main/webapp/src')
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: { '@ligoj/host': resolve(HOST_SRC, 'host.js'), '@': HOST_SRC },
+    dedupe: ['vue', 'pinia', 'vue-router', 'vuetify'],
+  },
+  build: {
+    lib: { entry: resolve(__dirname, 'src/index.js'), formats: ['es'], fileName: () => 'index.js' },
+    outDir: resolve(__dirname, '../src/main/resources/META-INF/resources/webjars/registry-harbor/vue'),
+    emptyOutDir: true,
+    rollupOptions: {
+      external: ['vue', 'vue-router', 'pinia', 'vuetify', '@ligoj/host'],
+      output: { assetFileNames: 'index.[ext]' },
+    },
+  },
+  server: { port: 5321, proxy: { '/rest': { target: 'http://localhost:8080', changeOrigin: true }, '/webjars': { target: 'http://localhost:8080', changeOrigin: true } } },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['src/__tests__/setup.js'],
+    exclude: ['node_modules/**', 'dist/**'],
+    css: false,
+    server: { deps: { inline: ['vuetify'] } },
+    coverage: {
+      provider: 'v8',
+      include: ['src/**/*.js'],
+      exclude: ['src/__tests__/**'],
+      thresholds: { statements: 100, branches: 100, functions: 100, lines: 100 },
+    },
+  },
+})